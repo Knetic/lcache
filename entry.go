2690df858f33237c1450693b8e8149234f88b075
choey <choey2k5@gmail.com>
2020-09-02 00:37:25 -0700

    gofmt

--- entry.go
+++ entry.go
@@ -5,18 +5,16 @@ import (
 )
 
 type cacheEntry struct {
-	
 	value interface{}
 
 	expiration time.Time
-	lastUsed time.Time
-	
-	refreshing bool
+	lastUsed   time.Time
+
+	refreshing   bool
 	refreshError error
 }
 
 func (this *cacheEntry) updateTimestamps(expireAfter time.Duration) {
-
 	this.lastUsed = time.Now()
 	if expireAfter > 0 {
 		this.expiration = this.lastUsed.Add(expireAfter)
--- loader.go
+++ loader.go
@@ -5,7 +5,6 @@ package lcache
 	Generally, the key will refer to something like a key name in Redis, or a a table+row in Cassandra.
 */
 type Loader interface {
-
 	// This will be called by the library at the point when a value needs to be loaded/reloaded from cache.
 	Load(key string) (interface{}, error)
 }
